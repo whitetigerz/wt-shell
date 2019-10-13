cf4b1ea38e6bc75e7816a4155d908e4186806e06
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-10-13 18:32:10 +0530

    Create wt-shell.sh

---
+++ wt-shell.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade
+apt install figlet
+apt install ruby
+apt install php
+gem install lolcat
+cd
+cd wt-shell
+mv whiteshell.enc whiteshell.php
+mv -v whiteshell.php $HOME
+figlet White Tigerz Shell |lolcat|lolcat
